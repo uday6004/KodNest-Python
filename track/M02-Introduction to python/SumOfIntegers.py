799c757234c74b301e31c1579facb290a6bee3bd
uday6004 <udaysh047@gmail.com>
2026-08-05 16:45:57 +0530

    total sum n number of pssitive integer

---
+++ track/M02-Introduction to python/SumOfIntegers.py
@@ -0,0 +1,13 @@
+# Read the value of n
+n=int(input("Enter n vlue:"))
+# Initialize the counter and total
+counter=1
+total=0
+# Calculate the total using a while loop
+while counter <=n :
+    total=total+counter
+    counter = counter+1
+# Display the total
+print("Total:",total)
+
+
