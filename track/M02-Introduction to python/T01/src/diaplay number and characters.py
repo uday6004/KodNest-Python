fc9789800e9fee9c9b962f552a098ed45866d52f
uday6004 <udaysh047@gmail.com>
2026-08-05 17:12:40 +0530

    update

---
+++ track/M02-Introduction to python/T01/src/diaplay number and characters.py
@@ -0,0 +1,11 @@
+# Read the number and word
+n=int(input())
+word=input()
+# Print the number sequence
+print("Numbers:")
+for i in range(1,n+1):
+print(i)
+# Print the characters
+print("Characters:")
+for char in word:
+print(char)
